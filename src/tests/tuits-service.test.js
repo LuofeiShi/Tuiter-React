3ea0783dd252e3e6543c0248a4216f1dfba77697
LuofeiShi <shiuofei@gmail.com>
2022-03-08 13:44:58 -0500

    implement tuit creation test

--- src/tests/tuits-service.test.js
+++ src/tests/tuits-service.test.js
@@ -1,24 +1,48 @@
-import {deleteUsersByUsername} from "../services/users-service";
+import {createUser, deleteUsersByUsername} from "../services/users-service";
+import {createTuit, deleteTuit} from "../services/tuits-service";
 
 describe('can create tuit with REST API', () => {
-  // TODO: implement this
-
     const ripley = {
         username: 'ellenripley',
         password: 'lv426',
         email: 'ellenripley@aliens.com'
     };
 
-    const repleyTuit = {
-        tuit: 'what is up this is ellen here',
-        
+    const testTuit = {
+        tuit: 'what is up this is ellen here'
     };
 
     // setup test before running test
     beforeAll(() => {
         // remove any/all users to make sure we create it in the test
         return deleteUsersByUsername(ripley.username);
-    })
+    });
+
+    afterAll(() => {
+        return deleteUsersByUsername(ripley.username);
+
+    });
+
+    test('can create tuit with createTuitByUser', async () => {
+        // insert user
+        const newUser = await createUser(ripley);
+
+        // verify inserted user properties match sample user
+        expect(newUser.username).toEqual(ripley.username);
+        expect(newUser.password).toEqual(ripley.password);
+        expect(newUser.email).toEqual(ripley.email);
+
+        // insert tuit
+        const newTuit = await createTuit(newUser._id, testTuit);
+
+        // verify inserted tuit
+        expect(newTuit.tuit).toEqual(testTuit.tuit);
+
+        // delete tuit
+        const status = await deleteTuit(newTuit._id);
+        // verify deleted
+        expect(status.deletedCount).toBeGreaterThanOrEqual(1);
+    });
 });
 
 describe('can delete tuit wtih REST API', () => {
