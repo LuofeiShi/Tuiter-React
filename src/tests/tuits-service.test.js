7e5b41264148fc0f21adfa3cdbb246b181174395
LuofeiShi <shiuofei@gmail.com>
2022-03-08 13:21:26 -0500

    update settings and test

--- src/tests/tuits-service.test.js
+++ src/tests/tuits-service.test.js
@@ -1,5 +1,24 @@
+import {deleteUsersByUsername} from "../services/users-service";
+
 describe('can create tuit with REST API', () => {
   // TODO: implement this
+
+    const ripley = {
+        username: 'ellenripley',
+        password: 'lv426',
+        email: 'ellenripley@aliens.com'
+    };
+
+    const repleyTuit = {
+        tuit: 'what is up this is ellen here',
+        
+    };
+
+    // setup test before running test
+    beforeAll(() => {
+        // remove any/all users to make sure we create it in the test
+        return deleteUsersByUsername(ripley.username);
+    })
 });
 
 describe('can delete tuit wtih REST API', () => {
