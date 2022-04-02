9ff4857b149b0b5a17843af557810bbdc3f3ed8d
Luofei Shi <shiluofei@gmail.com>
2022-04-02 02:52:42 -0400

    update to async functions

--- src/components/profile/my-likes.js
+++ src/components/profile/my-likes.js
@@ -4,8 +4,8 @@ import {useEffect, useState} from "react";
 
 const MyLikes = () => {
     const [likedTuits, setLikedTuis] = useState([]);
-    const findTuitsILike = () =>
-        service.findAllTuitsLikedByUser("me")
+    const findTuitsILike = async () =>
+        await service.findAllTuitsLikedByUser("me")
             .then((tuits) => setLikedTuis(tuits));
     useEffect(findTuitsILike, []);
     
