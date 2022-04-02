cf7d2f6209adbd47cab11d86466e0cbac75695d6
Luofei Shi <shiluofei@gmail.com>
2022-04-02 02:53:30 -0400

    update api addr

--- src/services/security-service.js
+++ src/services/security-service.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-// const BASE_URL = "https://cs5500-01-sp22.herokuapp.com/api";
-// const BASE_URL = "http://localhost:4000/api";
+
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const SECURITY_API = `${BASE_URL}/api/auth`;
@@ -23,8 +22,4 @@ export const logout = (user) =>
 
 export const profile = () =>
     api.post(`${SECURITY_API}/profile`)
-        .then(response => response.data);
-
-export const signup = (user) =>
-    api.post(`${AUTH_API}/signup`, user)
         .then(response => response.data);
