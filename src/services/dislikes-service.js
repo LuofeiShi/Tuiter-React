bfc8ee15d359ed4ef677657a7e62d5508055954d
Luofei Shi <shiluofei@gmail.com>
2022-04-02 18:20:46 -0400

    update jdoc

--- src/services/dislikes-service.js
+++ src/services/dislikes-service.js
@@ -1,3 +1,6 @@
+/**
+ * @file api service for dislike operations
+ */
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
@@ -8,18 +11,40 @@ const api = axios.create({
     withCredentials: true
 });
 
+/**
+ * Update the dislike stats of a tuit by a user
+ * @param uid primary key of user
+ * @param tid primary key of tuit
+ * @returns {Promise<AxiosResponse<any>>} dislike object
+ */
 export const userDislikesTuit = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
         .then(response => response.data);
 
+/**
+ * Check if a tuit is liked by a user
+ * @param uid primary key of the user
+ * @param tid primary key of the tuit
+ * @returns {Promise<AxiosResponse<any>>} json contains dislike boolean value
+ */
 export const tuitDislikedByMe = (uid, tid) =>
     api.get(`${USERS_API}/${uid}/dislikes/${tid}`)
         .then(response => response.data);
 
+/**
+ * Find all tuits that a user has disliked
+ * @param userId primary key of user
+ * @returns {Promise<AxiosResponse<any>>} json contains a list of tuits
+ */
 export const findAllTuitsDislikedByUser = (userId) =>
     api.get(`${USERS_API}/${userId}/dislikes`)
         .then(response => response.data);
 
+/**
+ * Find all users that dislike a given tuit
+ * @param tid primary key of tuit
+ * @returns {Promise<AxiosResponse<any>>} json contains a list of users
+ */
 export const findAllUsersThatDislikedTuit = (tid) =>
     api.get(`${TUITS_API}/${tid}/dislikes`)
         .then(response => response.data);
